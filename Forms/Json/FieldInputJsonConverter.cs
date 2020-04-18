fcf39f4b614e6511dafe37ac335d7159e17f66de
Timothy Jones <t.jones@aptacode.com>
2020-04-18 16:36:16 +0100

    Added old Json Converters

---
+++ Forms/Json/FieldInputJsonConverter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Aptacode.Forms.Elements.Fields;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace Aptacode.Forms.Json
+{
+    public class FieldInputJsonConverter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return typeof(FormField).IsAssignableFrom(objectType);
+        }
+
+        public override object ReadJson(JsonReader reader,
+            Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var jo = JObject.Load(reader);
+
+            var type = jo["Type"]?.Value<string>();
+
+            FormField item = null;
+
+            //switch (type)
+            //{
+            //    case nameof(TextFieldInput):
+            //        item = new TextFieldInput();
+            //        break;
+            //    case nameof(ComboBoxFieldInput):
+            //        item = new ComboBoxFieldInput();
+            //        break;
+            //    default:
+            //        return null;
+            //}
+
+            serializer.Populate(jo.CreateReader(), item);
+
+            return item;
+        }
+
+        public override bool CanWrite => false;
+
+        public override void WriteJson(JsonWriter writer,
+            object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
